afc0cd842e3e784be6dabc42196016feedce5f21
Ochieng John Ronnie <johnronnie18@gmail.com>
2024-01-02 12:19:25 +0300

    CRUD operations using SQLAlchemy

--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -11,3 +12,25 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def __repr__(self):
+        return f"({self.id}) {self.name} {self.breed}" if self.id else f"{self.name} {self.breed}"
+
+engine = create_engine('sqlite:///:memory:')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Osama_dog = Dog("Nyadala", "Nyaugenya")
+Sadam_dog = Dog("Onyango", "Jamigori")
+Chiwawa_dog = Dog("KRG", "The Donkey")
+session.bulk_save_objects([Osama_dog, Sadam_dog, Chiwawa_dog])
+session.commit()
+
+results = session.query(Dog).all()
+
